174e8291c7048e22176c5aa812c9f4b6169cc50e
oaniewiarowski <33526997+oaniewiarowski@users.noreply.github.com>
2021-05-07 18:51:57 -0400

    fixing small strain linear_membrane.py from jeremy - Qinv rather than Q, and wrong material matrix, adding benchmark data for future test cases

---
+++ bm_data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  7 18:36:16 2021
+
+@author: alexanderniewiarowski
+
+Wrinkling benchmark data
+"""
+import numpy as np
+
+class Mosler:
+    width = 200
+    height = 100
+    t = 0.2 
+    lamb = 1852.07
+    mu = 207.90
+
+class KannoIsotropic:
+    width = 200 # mm
+    height = 100
+    t = 0.025 # 25 um
+    E = E = 3500
+    nu = nu = 0.31
+    mu = mu = E/2/(1+nu)
+    lamb = lamb = E*nu/(1+nu)/(1-2*nu)
+    lamb_bar = 2*lamb*mu/(lamb+2*mu)
+
+
+
+class KannoOrthotropic:
+    E1 = 1E5
+    E2 = 10E5
+    v12 = 0.3
+    G = G = 0.4E5
+    C = np.array([[1/E1, -v12/E2, 0],
+                  [-v12/E2, 1/E2, 0],
+                  [0, 0, 1/(2*G)]])
+
+    C = np.linalg.inv(C)
+
+
--- linear_membrane.py
+++ linear_membrane.py
@@ -4,36 +4,62 @@
 Created on Thu Feb 11 11:18:24 2021
 
 @author: bleyerj
-"""
+Modified by a niewiarowski
 
+Case 1:
+    Topological Dimension: 2
+    Geometrical Dimension: 2
+    Coordinate System: Cartesian
+    Strain Measure: Small strain theory
+    Material Law: Linear (SVK)
+"""
 from fenics_optim import *
+from fenics_optim.quadratic_cones import get_slice
 import matplotlib.pyplot as plt
+import numpy as np
+from bm_data import *
+
+
+def C(lamb, mu):
+    return np.array([[lamb+2*mu, lamb, 0],
+                     [lamb, lamb+2*mu, 0],
+                     [0, 0, mu]])
+
+
+def C_plane_stress(E, nu):
+    return (E/(1-nu**2))*np.array([[1, nu, 0],
+                                   [nu, 1, 0],
+                                   [0, 0, (1-nu)/2]])
+
+
+
+bm = KannoIsotropic()
+np.testing.assert_array_almost_equal_nulp(C(bm.lamb_bar, bm.mu), C_plane_stress(bm.E, bm.nu))
+#%%
 
 N = 20
-mesh = UnitSquareMesh(N, N, "crossed")
-
+ux = 1
+uy = 1
+mesh = RectangleMesh(Point(0,0), Point(bm.width, bm.height), 2*N, N)#, "crossed")
 V = VectorFunctionSpace(mesh, "CG", 2)
+
 
 def left(x, on_boundary):
     return near(x[0], 0) and on_boundary
 def bottom(x, on_boundary):
     return near(x[1], 0) and on_boundary
 def top(x, on_boundary):
-    return near(x[1], 1) and on_boundary
+    return near(x[1], bm.height) and on_boundary
 def right(x, on_boundary):
-    return near(x[0], 1) and on_boundary
+    return near(x[0], bm.width) and on_boundary
 
-bc = [DirichletBC(V, Constant((0., 0.)), left),
-      DirichletBC(V, Constant((0.1,0.)), right)]
+bc = [DirichletBC(V, Constant((0, 0)), bottom),
+      DirichletBC(V, Constant((ux, uy)), top)]
 
-E = 10
-nu = 0.49
-mu = E/2/(1+nu)
-lamb = E*nu/(1+nu)/(1-2*nu)
-C = np.array([[lamb+2*mu, lamb, 0],
-              [lamb, lamb+2*mu, 0],
-              [0, 0, 2*mu]])
-Q = as_matrix(np.linalg.cholesky(C))
+#%%
+C = C_plane_stress(bm.lamb, bm.nu)
+Q = np.linalg.cholesky(C)
+Qinv = as_matrix(np.linalg.inv(Q))
 
 prob = MosekProblem("No-compression membrane model")
 u = prob.add_var(V, bc=bc)
@@ -43,7 +69,7 @@ class LinearMembrane(ConvexFunction):
     def conic_repr(self, X):
         y = self.add_var(dim=5, cone=RQuad(5), name="y")
         # elastic strain
-        Eel = dot(Q.T, get_slice(y, 2))
+        Eel = dot(Qinv.T, get_slice(y, 2))
         self.add_eq_constraint([None, y[1]], b=1)
         # wrinkling strain (up to a negative sign)
         Ew = self.add_var(dim=3, cone=SDP(2))
@@ -53,19 +79,19 @@ class LinearMembrane(ConvexFunction):
 
 
 energy = LinearMembrane(sym(grad(u)), degree=2)
-prob.add_convex_term(energy)
+prob.add_convex_term(Constant(bm.t)*energy)
 
 
 prob.parameters["presolve"] = True
 prob.optimize()
-
+#%
 plot(u, mode="displacement")
 plt.show()
 
 y = prob.get_var("y")
-
+#%%
 Veps = VectorFunctionSpace(mesh, "DG", 0, dim=3)
-Eel = project(dot(Q.T, get_slice(y, 2)), Veps, form_compiler_parameters={"quadrature_degree": energy.degree})
+Eel = project(dot(Qinv.T, get_slice(y, 2)), Veps, form_compiler_parameters={"quadrature_degree": energy.degree})
 Ew = project(to_vect(sym(grad(u))) - Eel, Veps)
 
 pl = plot(Ew[1])
