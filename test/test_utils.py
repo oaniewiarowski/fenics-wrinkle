f70ba15d614d81c6e61a51b1c1fe11cf74a98562
oaniewiarowski <33526997+oaniewiarowski@users.noreply.github.com>
2021-05-13 17:09:47 -0400

    adding functions for block ufl functionality, to_vect, and testing

---
+++ test/test_utils.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 13 15:03:21 2021
+
+@author: alexanderniewiarowski
+"""
+
+import unittest
+from dolfin import UnitSquareMesh, as_matrix, sqrt, assemble, inner, dx
+from wrinkle.utils import as_block_matrix
+
+
+class TestBlockMatrix(unittest.TestCase):
+
+    def setUp(self):
+        self.mesh = UnitSquareMesh(2, 2)
+        self.M = as_matrix([[1, 2],
+                            [3, 4]])
+        self.F = as_matrix([[11, 12],
+                            [13, 14],
+                            [20, 30]])
+
+    def test_non_symmetric_matrix(self):
+        # Test with non-symmetric matrix:
+        M = self.M
+        test = as_block_matrix([[M, -M],
+                                [-M, M]])
+
+        desired = as_matrix([[M[0, 0], M[0, 1],   -M[0, 0], -M[0, 1]],
+                             [M[1, 0], M[1, 1],   -M[1, 0], -M[1, 1]],
+
+                             [-M[0, 0], -M[0, 1],  M[0, 0], M[0, 1]],
+                             [-M[1, 0], -M[1, 1],  M[1, 0], M[1, 1]]])
+        err = test - desired
+        self.assertEqual(sqrt(assemble(inner(err, err)*dx(domain=self.mesh))),
+                         0, 'The arrays are not equal!')
+
+    def test_non_square_matrix(self):
+        # Test with non-square matrix:
+        F = self.F
+        test = as_block_matrix([[F, -F],
+                                [-F, F]])
+        desired = as_matrix([[F[0, 0], F[0, 1],   -F[0, 0], -F[0, 1]],
+                             [F[1, 0], F[1, 1],   -F[1, 0], -F[1, 1]],
+                             [F[2, 0], F[2, 1],   -F[2, 0], -F[2, 1]],
+
+                             [-F[0, 0], -F[0, 1],  F[0, 0], F[0, 1]],
+                             [-F[1, 0], -F[1, 1],  F[1, 0], F[1, 1]],
+                             [-F[2, 0], -F[2, 1],  F[2, 0], F[2, 1]]])
+        err = test - desired
+        self.assertEqual(sqrt(assemble(inner(err, err)*dx(domain=self.mesh))),
+                         0, 'The arrays are not equal!')
+
+    def test_variable_size_matrix(self):
+        I3 = -as_matrix([[1, 2, 3],
+                         [4, 5, 6],
+                         [7, 8, 9]])
+        M = self.M
+        F = self.F
+        test = as_block_matrix([[M, -F.T],
+                                [-F, I3]])
+        desired = as_matrix([
+            [M[0, 0], M[0, 1],    -F[0, 0], -F[1, 0], -F[2, 0]],
+            [M[1, 0], M[1, 1],    -F[0, 1], -F[1, 1], -F[2, 1]],
+
+            [-F[0, 0], -F[0, 1],  I3[0, 0], I3[0, 1], I3[0, 2]],
+            [-F[1, 0], -F[1, 1],   I3[1, 0], I3[1, 1], I3[1, 2]],
+            [-F[2, 0], -F[2, 1],   I3[2, 0], I3[2, 1], I3[2, 2]]])
+        err = test - desired
+        self.assertEqual(sqrt(assemble(inner(err, err)*dx(domain=self.mesh))),
+                         0, 'The arrays are not equal!')
+
+    def test_block_to_vec(self):
+        from wrinkle.utils import to_vect
+        I3 = -as_matrix([[1, 2, 3],
+                         [4, 5, 6],
+                         [7, 8, 9]])
+        M = self.M
+        F = self.F
+        test = to_vect(as_block_matrix([[M, -F.T],
+                                        [-F, I3]]))
+        from dolfin import as_vector
+        desired = as_vector([M[0, 0], M[1, 1], I3[0, 0], I3[1, 1], I3[2, 2],
+                             M[0, 1],  -F[0, 1], I3[0, 1], I3[1, 2],
+                             -F[0, 0],  -F[1, 1], I3[0, 2],
+                             -F[1, 0], -F[2, 1],
+                             -F[2, 0]])
+
+        err = test - desired
+        from dolfin import VectorFunctionSpace, project
+        V = VectorFunctionSpace(self.mesh, 'CG', 1, dim=15)
+        des = project(desired, V)(.5, .5)
+        act = project(test, V)(.5, .5)
+        errmsg = f'The arrays are not equal! Desired={print(des)}, \n \
+            actual={print(act)}'
+        self.assertEqual(sqrt(assemble(inner(err, err)*dx(domain=self.mesh))),
+                         0, errmsg)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- utils.py
+++ utils.py
@@ -6,7 +6,8 @@ Created on Tue Apr 13 14:25:40 2021
 @author: alexanderniewiarowski
 """
 
-from dolfin import tr, det, DOLFIN_EPS_LARGE, conditional, sqrt, gt, as_vector, dot
+from dolfin import tr, det, DOLFIN_EPS_LARGE, conditional, sqrt, gt, as_vector, dot, as_matrix
+from ufl import shape
 
 
 def eigenvalue(A):
@@ -51,3 +52,50 @@ def eig_vecmat(A):
     v2 = v2/sqrt(dot(v2, v2))
 
     return v1, v2
+
+
+def to_vect(X):
+    """ Transforms symmetric tensor into vector by spanning upper diagonals """
+    s = shape(X)
+    if len(s)==2 and s[0] == s[1]:
+        d = s[0]
+        return as_vector([X[i, i+k]
+                          for k in range(d) for i in range(d-k)])
+    else:
+        raise(ValueError, "Variable must be a square tensor")
+
+
+def as_block_matrix(mlist):
+    rows = []
+    # Block rows:
+    for i in range(0, len(mlist)):
+        block_row_height = shape(mlist[i][0])[0]
+        # Matrix rows in block row i:
+        for j in range(0, block_row_height):
+            row = []
+            # Block columns:
+            for k in range(0, len(mlist[i])):
+                block_column_width = shape(mlist[i][k])[1]
+                # Matrix columns in block column k:
+                for l in range(0, block_column_width):
+                    row += [mlist[i][k][j, l], ]
+            rows += [row, ]
+    return as_matrix(rows)
+
+
+# def diagonalize(A):
+#     '''fenics_optim.to_vect spans lower diagonal'''
+#     ROW, COL = np.shape(A)
+#     vec = []
+#     for j in range(COL):
+#         for i in range(ROW - j):
+#             vec.append(A[i, i+j])
+#     return vec
+
+# import numpy as np
+# test = [[0, 1, 2, 3],
+#         [4, 5, 6, 7],
+#         [7, 8, 9, 10],
+#         [11, 12, 13, 14]]
+# print(diagonalize(np.array(test)))
+# print(to_vect(as_matrix(test)))
