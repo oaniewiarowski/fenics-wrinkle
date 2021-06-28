6c4531f42abdba526c1eb1112a3d8ebc8d730d5e
oaniewiarowski <33526997+oaniewiarowski@users.noreply.github.com>
2021-06-28 00:31:09 -0400

    cleaning up hypar example, membrane state plotting

--- examples/hypar.py
+++ examples/hypar.py
@@ -6,29 +6,23 @@ Created on Fri May 14 17:02:43 2021
 @author: alexanderniewiarowski
 """
 
-from fenics_optim import *
-from fenics_optim.quadratic_cones import get_slice
-from fenicsmembranes.parametric_membrane import *
-import matplotlib.pyplot as plt
-import numpy as np
-from fenics_wrinkle.bm_data import *
+import fenics_optim as fo
+import dolfin as df
+from dolfin import project, DirichletBC, near, Constant
+from fenicsmembranes.parametric_membrane import ParametricMembrane
+from fenics_wrinkle.bm_data import KannoIsotropic
 from fenics_wrinkle.materials.INH import INHMembrane
-# from fenics_wrinkle.materials.svk import NonLinearMembrane
-
-from fenics_wrinkle.utils import expand_ufl
 from fenics_wrinkle.io import WrinklePlotter
 
 bm = KannoIsotropic()
-np.testing.assert_array_almost_equal_nulp(C(bm.lamb_bar, bm.mu), C_plane_stress(bm.E, bm.nu))
 
-C = C_plane_stress(bm.lamb, bm.nu)
-
-width = 10
-height = 10
-H = 5 # vertical displacement of the hypar
-N = 40  # mesh resolution
-c = 2  #  in-plane stretching
-mesh = RectangleMesh(Point(0,0), Point(width, height), N, N, 'crossed')
+QUAD_DEGREE = 3
+width = w = 10/df.sqrt(2)
+height = h = 10/df.sqrt(2)
+H = 5/df.sqrt(2)  # vertical displacement of the hypar
+N = 30  # mesh resolution
+c = 1/df.sqrt(2)  # in-plane stretching
+mesh = df.RectangleMesh(df.Point(0, 0), df.Point(width, height), N, N, 'crossed')
 
 def NE(x):
     return near(x[0], width) and near(x[1], height)
@@ -39,6 +33,7 @@ def SW(x):
 def NW(x):
     return near(x[0], 0) and near(x[1], height)
 
+
 def bc(membrane):
     bc = [DirichletBC(membrane.V, Constant((c, -c, 0)), SE, method='pointwise'),
           DirichletBC(membrane.V, Constant((-c, c, 0)), NW, method='pointwise'),
@@ -46,150 +41,92 @@ def bc(membrane):
           DirichletBC(membrane.V, Constant((c, c, H)), NE, method='pointwise')]
     return bc
 
+#%%
+import mshr
 
+b = .25/df.sqrt(2)
+bb = b/df.sqrt(2)
+d = df.sqrt(w**2 + h**2)
+pts = [[b, 0],
+       [w-b, 0],
+       [w,b],
+       [w,h-b],
+       [w-b,h],
+       [b,h],
+       [0,h-b],
+       [0,b]]
+domain = mshr.Polygon([df.Point(pt) for pt in pts])
+mesh = mshr.generate_mesh(domain,N)
+mesh.rotate(-45, 2, df.Point(b/2,b/2))
+mesh.translate(df.Point((-b/2, d/2-bb-b/2)))
+
+eps=1e-6
+bot = df.CompiledSubDomain("(near(x[1], 0, eps) && on_boundary)", eps=eps)
+top = df.CompiledSubDomain("(near(x[1], ymax, eps) && on_boundary)", ymax=(d-2*bb), eps=eps)
+left = df.CompiledSubDomain("(near(x[0], 0, eps) && on_boundary)", eps=eps)
+right = df.CompiledSubDomain("(near(x[0], xmax, eps) && on_boundary)", xmax=(d-2*bb), eps=eps)
+
+
+def bc(membrane):
+    bc = [DirichletBC(membrane.V, Constant((c, 0, 0)), right),
+          DirichletBC(membrane.V, Constant((-c, 0, 0)), left),
+          DirichletBC(membrane.V, Constant((0, -c, H)), bot),
+          DirichletBC(membrane.V, Constant((0, c, H)), top)]
+    return bc
+
+
+
+mesh_func = df.MeshFunction("size_t", mesh, mesh.topology().dim()-1)
+
+bot.mark(mesh_func, 1)
+top.mark(mesh_func, 2)
+left.mark(mesh_func, 3)
+right.mark(mesh_func, 4)
+
+df.File('output/mesh_func.pvd') << mesh_func
+# mem.ds = df.Measure('ds', domain=mem.mesh, subdomain_data=mesh_func)
+
+
+#%%
 class Geometry:
     def __init__(self):
-        V = VectorFunctionSpace(mesh, 'CG', 1, dim=3)
-        self.gamma = project(Expression(('x[0]', 'x[1]', 0), degree=1), V)
+        V = df.VectorFunctionSpace(mesh, 'CG', 1, dim=3)
+        self.gamma = project(df.Expression(('x[0]', 'x[1]', 0), degree=1), V)
         self.Gsub1 = project(Constant((1, 0, 0)), V)
         self.Gsub2 = project(Constant((0, 1, 0)), V)
+
 
 geo = Geometry()
 
 input_dict = {
         'mesh': mesh,
-        'resolution': [30,30],
         'geometry': geo,
         'thickness': bm.t,
         'material': 'Incompressible NeoHookean',
         'mu': bm.mu,
-        'cylindrical': True,
-        'output_file_path': 'hypar',
+        'cylindrical': False,
+        'output_file_path': 'output/hypar',
         'pressure': 0,
         'Boundary Conditions': bc}
 
 mem = membrane = ParametricMembrane((input_dict))
 mem.io.write_fields()
 
-prob = MosekProblem("No-compression membrane model")
+prob = fo.MosekProblem("No-compression membrane model")
 u__ = prob.add_var(membrane.V, bc=membrane.bc)
 prob.var[0] = membrane.u   # replace
 u = membrane.u
 
-energy = INHMembrane(u, mem, degree=2)
-# prob.add_convex_term(Constant(bm.t)*energy)
+energy = INHMembrane(u, mem, degree=QUAD_DEGREE)
 prob.add_convex_term(bm.t*bm.mu/2*energy)
 io = WrinklePlotter(mem, energy)
-mem.io.add_plotter(io.plot, 'output/xdmf_write_interval', 0)   
+mem.io.add_plotter(io.plot, 'output/xdmf_write_interval', 0)
 prob.parameters["presolve"] = True
 
 prob.optimize()
-mem.io.write_fields()
-
-#%%
-class PressureLoad(ConvexFunction):
-
-    def __init__(self, u, mem, **kwargs):
-        self.mem = mem
-        ConvexFunction.__init__(self, u, parameters=None, **kwargs)
-
-    def conic_repr(self, u):
-        n_star = self.add_var(dim=3, cone=None, name='pressure')
-        n = as_vector([n_star[0], n_star[1], n_star[2]])
-        g3 = as_vector([-u[2].dx(0) - u[2].dx(0)*u[1].dx(1) + u[1].dx(0)*u[2].dx(1), 
-                        u[2].dx(0)*u[0].dx(1) - u[2].dx(1) - u[0].dx(0)*u[2].dx(1),
-                        u[0].dx(0) + u[1].dx(1) +u[0].dx(0)*u[1].dx(1) - u[1].dx(0)*u[0].dx(1)])
-        b = as_vector([0,0,-1])
-        self.add_eq_constraint([-g3, None, None,None, n], b=-b)
-        self.set_linear_term([None, None, None, None, -dot(n,u)])
-# p = PressureLoad(u, mem)
-# prob.add_convex_term(-dot(mem.gsub3,u)*dx(mem.mesh))
-# prob.add_convex_term(p)
-prob.optimize()
-mem.io.write_fields()
-
-#%%
-from utils import eigenvalue, eig_vecmat
-# E = membrane.E
-# E_el = as_tensor([[energy.Eel[0],energy.Eel[2]],
-#                   [energy.Eel[2],energy.Eel[1]]])
-
-# # Veps = VectorFunctionSpace(mesh, "DG", 0, dim=3)
-# # E_el = project(energy.Eel, Veps, form_compiler_parameters={"quadrature_degree": energy.degree})
-# # E_w = project(E - Eel, Veps)
-# E_w = E - E_el
-
-E = membrane.E
-E_el = 0.5*(energy.C_elastic - membrane.C_0)
-E_w = (E - E_el)
-
-def write_eigval(A, l1_name, l2_name):
-    l1, l2 = eigenvalue(A)
-    
-    l1 = project(l1, FunctionSpace(mem.mesh, 'CG',1), form_compiler_parameters={"quadrature_degree": energy.degree})
-    l1.rename(l1_name, l1_name)
-    
-    l2 = project(l2, FunctionSpace(mem.mesh, 'CG',1), form_compiler_parameters={"quadrature_degree": energy.degree})
-    l2.rename(l2_name, l2_name)
-    
-    mem.io.add_extra_output_function(l1)
-    mem.io.add_extra_output_function(l2)
-    # mem.io.write_fields()
-    
 
 
-def write_eigvec(A, v1_name, v2_name):
-    v1, v2 = eig_vecmat(A)
-    S = VectorFunctionSpace(mem.mesh, 'CG', 1)
-    eig1 = project(v1, S, form_compiler_parameters={"quadrature_degree": energy.degree})
-    eig1.rename(v1_name, v1_name)
-    eig2 = project(v2, S, form_compiler_parameters={"quadrature_degree": energy.degree})
-    eig2.rename(v2_name, v2_name)
-    mem.io.add_extra_output_function(eig1)
-    mem.io.add_extra_output_function(eig2)
-    # mem.io.write_fields()
-
-
-#%
-J = det(energy.C_elastic)/det(mem.C_0)
-
-import ufl
-i,j = ufl.indices(2)
-gsup = mem.get_metric(mem.gsup1, mem.gsup2)
-gsup = inv(energy.C_elastic)
-sigma = as_tensor(bm.mu/J*(mem.C_0_sup[i,j] - 1/J**2*gsup[i,j]), [i,j] )
-
-sigma = S = as_tensor(bm.mu*(mem.C_0_sup[i,j] - 1/J*gsup[i,j]), [i,j] )
-# sigma = mem.F_n*S*mem.F_n.T
-write_eigval(sigma, 'sigma1', 'sigma2')
-write_eigvec(sigma, 'sigma1v', 'sigma2v')
-
-
-
-
-write_eigval(E_w, 'Ew1', 'Ew2')
-write_eigvec(E_w, 'Ew_v1', 'Ew_v2')
-
-write_eigval(E_el, 'E_el1', 'E_el2')
-write_eigvec(E_el, 'E_el_eig1', 'E_el_eig2')
-
-
-
-# 2PK S
-gsup = inv(energy.C_elastic)
-S = as_tensor(bm.mu*(mem.C_0_sup[i,j] - 1/J*gsup[i,j]), [i,j] )
-s1, s2 = eigenvalue(S)
-sv1, sv2 = eig_vecmat(S)
-
-# drop z comp of Fn
-Fn = as_tensor([[mem.gsub1[0], mem.gsub1[1]],
-                [mem.gsub2[0], mem.gsub2[1]]]).T
-
-sigma_el = (s1/J)*outer(dot(Fn,sv1), dot(Fn,sv1))
-
-write_eigval(sigma_el, 'sigma1_el', 'sigma2_el')
-write_eigvec(sigma_el, 'sigma1v_el', 'sigma2v_el')
-
-
-
+io = WrinklePlotter(mem, energy)
+io.s1_max =Constant(420)
+mem.io.add_plotter(io.plot, 'output/xdmf_write_interval', 0)
 mem.io.write_fields()
--- fenics_wrinkle/io.py
+++ fenics_wrinkle/io.py
@@ -5,8 +5,8 @@ Created on Fri Jun 18 14:37:32 2021
 
 @author: alexanderniewiarowski
 """
-
-from dolfin import *
+import dolfin as df
+from dolfin import as_vector, project, FunctionSpace, VectorFunctionSpace
 import ufl
 from fenics_wrinkle.utils import eigenvalue, eig_vecmat
 
@@ -18,7 +18,8 @@ class WrinklePlotter:
         self.energy = energy
         self.fcp = {"quadrature_degree": energy.degree}
         self.Vs = FunctionSpace(mem.mesh, 'CG', 1)
-        self.V = VectorFunctionSpace(mem.mesh, 'CG', 1)
+        self.V = VectorFunctionSpace(mem.mesh, 'CG', 1, dim=2)
+        self.V3 = VectorFunctionSpace(mem.mesh, 'CG', 1, dim=3)
 
         E_el = energy.E_el
         E_w = energy.E_w
@@ -27,12 +28,13 @@ class WrinklePlotter:
         E_el1, E_el2 = eigenvalue(E_el)
         E_w1, E_w2 = eigenvalue(E_w)
         sigma_1, sigma_2 = eigenvalue(energy.get_cauchy_stress())
+
         self.scalar_fields = {'E_el1': E_el1,
                               'E_el2': E_el2,
                               'E_w1': E_w1,
                               'E_w2': E_w2,
                                'sigma_1': sigma_1,
-                               'sigma_2': sigma_2
+                               'sigma_2': sigma_2,
                               }
 
         self.data = {}
@@ -46,17 +48,31 @@ class WrinklePlotter:
         E_el_v1, E_el_v2 = eig_vecmat(E_el)
         E_w_v1, E_w_v2 = eig_vecmat(E_w)
         sigma_v1, sigma_v2 = eig_vecmat(energy.get_cauchy_stress())
+
+        # project 2x1 eigenvectors onto deformed surface for plotting
+        # strangely this also works is sigma is 2x1... why?
+        norm_vec = lambda v: v/df.sqrt(df.dot(v, v))
+        P = df.Identity(3) - df.outer(mem.n, mem.n)  # projection tensor
+        sigma_v1_3d = norm_vec(P*as_vector([sigma_v1[0], sigma_v1[1], 0]))
+        sigma_v2_3d = norm_vec(P*as_vector([sigma_v2[0], sigma_v2[1], 0]))
+
         self.vector_fields = {'E_el_v1': E_el_v1,
                               'E_el_v2': E_el_v2,
                               'E_w_v1': E_w_v1,
                               'E_w_v2': E_w_v2,
-                               'sigma_v1': sigma_v1,
-                               'sigma_v2': sigma_v2
+                              'sigma_v1': sigma_v1,
+                              'sigma_v2': sigma_v2,
+                               'sigma_v1_3d': sigma_v1_3d,
+                               'sigma_v2_3d': sigma_v2_3d
                               }
 
         for name, field in self.vector_fields.items():
+            if field.ufl_shape == (2, ):
+                V = self.V
+            else:
+                V = self.V3
             self.data[name] = project(field,
-                                      self.V,
+                                      V,
                                       form_compiler_parameters=self.fcp)
             self.data[name].rename(name, name)
 
@@ -69,45 +85,76 @@ class WrinklePlotter:
             self.data[name].assign(project(field,
                                            self.Vs,
                                            form_compiler_parameters=self.fcp))
-            
+
         for name, field in self.vector_fields.items():
+            if field.ufl_shape == (2, ):
+                V = self.V
+            else:
+                V = self.V3
+
             self.data[name].assign(project(field,
-                                           self.V,
+                                           V,
                                            form_compiler_parameters=self.fcp))
 
         for func in self.data.values():
-                # fix t-1 in main io (t incremented in primary plotter func)
-                xdmf.xdmf_file.write(func, xdmf.t-1)  
-                
-                
+            # fix t-1 in main io (t incremented in primary plotter func)
+            xdmf.xdmf_file.write(func, xdmf.t-1)
+
+        s1, s2 = eigenvalue(energy.get_cauchy_stress())
+        if not hasattr(self, 's1_max'):
+            s1_vertex_vals = project(s1, self.Vs,
+                                     form_compiler_parameters=self.fcp
+                                     ).compute_vertex_values()
+            self.s1_max = df.Constant(max(s1_vertex_vals))
+
+        s1_max = self.s1_max
+        thresh = df.Constant(1e-2)  # df.Constant(0.01)
+
+        # s_1 > thresh*s1_max ?
+        s1_gt = df.gt(s1, thresh*s1_max)
+        s1_lt = df.lt(s1, thresh*s1_max)
+
+        # s_2 /max{1, s1} > thresh?
+        s2_gt = df.gt(s2/ufl.Max(df.Constant(1), s1), thresh)
+        s2_lt = df.lt(s2/ufl.Max(df.Constant(1), s1), thresh)
+
+        self.state = df.conditional(ufl.And(s1_gt, s2_gt), 2,
+                                    df.conditional(ufl.And(s1_gt, s2_lt),
+                                                   1, 0))
+
+        self.state_func = project(self.state,
+                                  self.Vs,
+                                  form_compiler_parameters=self.fcp)
+        self.state_func.rename('state', 'state')
+        xdmf.xdmf_file.write(self.state_func, xdmf.t-1)
+
+        self.state_func = project(self.state,
+                                  df.FunctionSpace(mem.mesh, 'DG', 0),
+                                  form_compiler_parameters=self.fcp)
+        self.state_func.rename('statedg', 'statedg')
+        xdmf.xdmf_file.write(self.state_func, xdmf.t-1)
+
     def write_eigval(self, A, l1_name, l2_name):
         mem = self.mem
-        energy = self.energy
         l1, l2 = eigenvalue(A)
-        
-        l1 = project(l1, FunctionSpace(mem.mesh, 'CG',1), form_compiler_parameters={"quadrature_degree": energy.degree})
+
+        l1 = project(l1, self.Vs, form_compiler_parameters=self.fcp)
         l1.rename(l1_name, l1_name)
-        
-        l2 = project(l2, FunctionSpace(mem.mesh, 'CG',1), form_compiler_parameters={"quadrature_degree": energy.degree})
+
+        l2 = project(l2, self.Vs, form_compiler_parameters=self.fcp)
         l2.rename(l2_name, l2_name)
-        
+
         mem.io.add_extra_output_function(l1)
         mem.io.add_extra_output_function(l2)
         mem.io.write_fields()
-        
-    
+
     def write_eigvec(self, A, v1_name, v2_name):
         mem = self.mem
-        energy = self.energy
         v1, v2 = eig_vecmat(A)
-        S = VectorFunctionSpace(mem.mesh, 'CG', 1)
-        eig1 = project(v1, S, form_compiler_parameters={"quadrature_degree": energy.degree})
+        eig1 = project(v1, self.V3, form_compiler_parameters=self.fcp)
         eig1.rename(v1_name, v1_name)
-        eig2 = project(v2, S, form_compiler_parameters={"quadrature_degree": energy.degree})
+        eig2 = project(v2, self.V3, form_compiler_parameters=self.fcp)
         eig2.rename(v2_name, v2_name)
         mem.io.add_extra_output_function(eig1)
         mem.io.add_extra_output_function(eig2)
         mem.io.write_fields()
-        
-    
-
